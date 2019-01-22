0657e077f185d1e615cb5a20a42d4df4f052af64
Jabeen Cheema <jabeencheema@pursuit.org>
2019-01-22 17:13:27 -0500

    added image helper

---
+++ WeatherApp/WeatherApp/View/FavsCell.swift
@@ -0,0 +1,28 @@
+//
+//  FavsCell.swift
+//  WeatherApp
+//
+//  Created by Jabeen's MacBook on 1/22/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class FavsCell: UITableViewCell {
+
+    @IBOutlet weak var favoriteImageView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
